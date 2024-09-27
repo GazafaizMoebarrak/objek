e9c6538fabd6fd4f0915bc3fc41d0423841c6894
GazafaizMoebarrak <gmoebarrak@gmail.com>
2024-09-27 09:07:52 +0700

    Add files via upload

---
+++ Objek2/src/main/java/com/mycompany/objek2/Objek2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.objek2;
+
+/**
+ *
+ * @author DH
+ */
+public class Objek2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
